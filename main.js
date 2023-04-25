4d25dfa871e2d80e3549a1ae3a34864b61959569
Rites1 <103243296+Rites1@users.noreply.github.com>
2023-04-25 20:52:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 https://teachablemachine.withgoogle.com/models/stHHtDi_t/
+function start(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/stHHtDi_t/model.json', modelready);
+}
+function modelready(){
+    classifier.classify(gotResults);
+}
+function gotResults(error, results){
+    console.log("gotResult");
+}
